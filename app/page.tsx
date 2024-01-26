a57ff3b7c28dfbd460c65ca9ff719233956179fd
Niels Houben <niels.houben1@gmail.com>
2024-01-26 20:31:30 +0100

    socketio chat function

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,19 @@
 
 import { Button } from "@/components/ui/button";
 import { signOut } from "next-auth/react";
+import { trpc } from "./_trpc/client";
 
-export default function Home() {
+export default function Home () {
+  const x = trpc.example.example.useMutation()
+  async function test () {
+    // console.log(x)
+    let res = await x.mutateAsync()
+    console.log(res)
+  }
+
   return (
     <div>
-      <Button className="font-semibold">Shush</Button>
+      <Button onClick={() => void test()} className="font-semibold">Shush</Button>
     </div>
   );
 }
