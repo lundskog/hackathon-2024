525ec665b0bc02bd51b2c4be5751359f2f83969c
bananielskoglund <bananielskoglund@gmail.com>
2024-01-26 19:50:27 +0100

    kk

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,22 @@
 
 import { Button } from "@/components/ui/button";
 import { signOut } from "next-auth/react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Button className="font-semibold">Shush</Button>
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="font-bold text-7xl">Allgood.cards</h1>
+        <div className="flex gap-2">
+          <Link href={"/games/create"}>
+            <Button className="font-semibold w-fit">Create game</Button>
+          </Link>
+          <Button variant={"secondary"} className="font-semibold w-fit">
+            Read more
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ server/api/routers/game.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "@/server/api/trpc";
+import { users } from "@/db/schema";
+
+export const gameRouter = createTRPCRouter({
+  create: publicProcedure.mutation(async ({ ctx }) => {}),
+});
