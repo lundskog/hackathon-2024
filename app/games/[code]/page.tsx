cd3cfd3f1c8bd6cb7f5cb110a06bbb1489259f9d
bananielskoglund <bananielskoglund@gmail.com>
2024-01-27 16:20:53 +0100

    room socket connect started

--- app/games/[code]/page.tsx
+++ app/games/[code]/page.tsx
@@ -1,24 +1,45 @@
 "use client";
 import { trpc } from "@/app/_trpc/client";
+import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { io, type Socket } from "socket.io-client";
 
 export default function GamePage() {
   const pathnameList = usePathname()?.split("/");
   const gameCode = pathnameList?.at(-1);
 
+  const [socket, setSocket] = useState<Socket>();
+  const [gameId, setGameId] = useState<string>("");
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (!socket) {
+      const newSocket = io("http://localhost:3001");
+      setSocket(newSocket);
+    } else {
+      socket.emit("join_room", gameCode);
+    }
+  }, [socket]);
+
   const { data: game } = trpc.games.get.useQuery({
     gameCode: gameCode ?? "",
   });
 
-  if (game) {
+  const NicknamePrompt = () => {
+    return <></>;
+  };
+
+  if (game && socket) {
     const GuestInLocal = game.users.filter(
       (user) => user.id === localStorage.getItem("playerId")
-    );
-    if (GuestInLocal) {
+    )[0];
+
+    if (GuestInLocal || session) {
       return (
         <div>
           <div>
+            {socket.connected ? "Connected" : "?"}
             <h1 className="font-semibold text-4xl">{game.name}</h1>
             <p className="font-semibold text-muted-foreground">{game.phase}</p>
           </div>
--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com"]
+    domains: ["avatars.githubusercontent.com"],
   },
-}
+  reactStrictMode: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
