41d3af686d30a9aae3ec54b466c79f36a016d8b3
bananielskoglund <bananielskoglund@gmail.com>
2024-01-27 16:58:58 +0100

    shsh

--- app/games/[code]/page.tsx
+++ app/games/[code]/page.tsx
@@ -1,9 +1,22 @@
 "use client";
 import { trpc } from "@/app/_trpc/client";
 import { useSession } from "next-auth/react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { io, type Socket } from "socket.io-client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Input } from "@/components/ui/input";
 
 export default function GamePage() {
   const pathnameList = usePathname()?.split("/");
@@ -11,7 +24,9 @@ export default function GamePage() {
 
   const [socket, setSocket] = useState<Socket>();
   const [gameId, setGameId] = useState<string>("");
+  const [player, setPlayer] = useState();
   const { data: session } = useSession();
+  const router = useRouter();
 
   useEffect(() => {
     if (!socket) {
@@ -26,25 +41,43 @@ export default function GamePage() {
     gameCode: gameCode ?? "",
   });
 
+  function handleJoin() {}
+
   const NicknamePrompt = () => {
-    return <></>;
+    const [routerOpen, setRouterOpen] = useState<boolean>(true);
+    return (
+      <AlertDialog open={routerOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Choose nickname</AlertDialogTitle>
+            <Input placeholder={"Niels Houben"} />
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={() => router.push("/")}>
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction>Join</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    );
   };
 
-  if (game && socket) {
+  if (game && socket && socket.connected) {
     const GuestInLocal = game.users.filter(
       (user) => user.id === localStorage.getItem("playerId")
     )[0];
 
-    if (GuestInLocal || session) {
-      return (
+    return (
+      <div>
+        {GuestInLocal || game.creatorId === session?.user.id ? null : (
+          <NicknamePrompt />
+        )}
         <div>
-          <div>
-            {socket.connected ? "Connected" : "?"}
-            <h1 className="font-semibold text-4xl">{game.name}</h1>
-            <p className="font-semibold text-muted-foreground">{game.phase}</p>
-          </div>
+          <h1 className="font-semibold text-4xl">{game.name}</h1>
+          <p className="font-semibold text-muted-foreground">{game.phase}</p>
         </div>
-      );
-    }
+      </div>
+    );
   }
 }
--- server/api/routers/game.ts
+++ server/api/routers/game.ts
@@ -4,62 +4,77 @@ import {
   publicProcedure,
   protectedProcedure,
 } from "@/server/api/trpc";
-import { decksToGames, usersToGame, games, insertGameSchema, users } from "@/db/schema";
+import {
+  decksToGames,
+  usersToGame,
+  games,
+  insertGameSchema,
+  users,
+} from "@/db/schema";
 import { v4 } from "uuid";
 import { eq } from "drizzle-orm";
 import { randomNumbers } from "@/lib/utils";
 
 export const gameRouter = createTRPCRouter({
-  create: publicProcedure
-    .input(z.object({
-      game: insertGameSchema,
-      selectedDecks: z.array(z.string())
-    }))
+  create: protectedProcedure
+    .input(
+      z.object({
+        game: insertGameSchema,
+        selectedDecks: z.array(z.string()),
+      })
+    )
     .mutation(async ({ ctx, input }) => {
-      const words: string[] = await fetch("https://random-word-api.vercel.app/api?words=3").then(res => res.json())
-      const code = words.join("-") + "-" + randomNumbers(3)
+      const words: string[] = await fetch(
+        "https://random-word-api.vercel.app/api?words=3"
+      ).then((res) => res.json());
+      const code = words.join("-") + "-" + randomNumbers(3);
       const { discoverability, name, id } = input.game;
-      const selectedDecks = input.selectedDecks
+      const selectedDecks = input.selectedDecks;
       await ctx.db.insert(games).values({
         id,
         name,
         code,
         discoverability,
-      })
-      await ctx.db.insert(decksToGames).values(selectedDecks.map(deck => {
-        return ({
-          deckId: deck,
-          gameId: id,
+        creatorId: ctx.session.user.id,
+      });
+      await ctx.db.insert(decksToGames).values(
+        selectedDecks.map((deck) => {
+          return {
+            deckId: deck,
+            gameId: id,
+          };
         })
-      }))
-      return code
+      );
+      return code;
     }),
   createPlayer: publicProcedure
-    .input(z.object({
-      gameId: z.string(),
-      nickname: z.string(),
-
-    }))
+    .input(
+      z.object({
+        gameId: z.string(),
+        nickname: z.string(),
+      })
+    )
     .mutation(async ({ ctx, input }) => {
       await ctx.db.insert(usersToGame).values({
         id: v4(),
         gameId: input.gameId,
         nickname: input.nickname,
-        userId: ctx.session?.user.id
-      })
+        userId: ctx.session?.user.id,
+      });
     }),
   get: publicProcedure
-    .input(z.object({
-      gameCode: z.string()
-    }))
+    .input(
+      z.object({
+        gameCode: z.string(),
+      })
+    )
     .query(async ({ ctx, input }) => {
-
       const res = await ctx.db.query.games.findFirst({
         where: eq(games.code, input.gameCode),
         with: {
-          users: true
-        }
-      })
+          users: true,
+        },
+      });
       return res;
-    })
+    }),
 });
