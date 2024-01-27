9d31c119039b64e5365cf059a85c662044c1c9af
Arvid Kolsmyr <kolsmyrarvid@gmail.com>
2024-01-27 13:09:51 +0100

    created slid-in-rotate animation

--- components/BlackCard.tsx
+++ components/BlackCard.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function BlackCard() {
   return (
-    <div className='w-72 h-96 text-black-400 bg-orange-500 rounded-md flex items-center justify-center transition delay-150 duration-300 ease-in-out -translate-y-100 translate-y-200'>BlackCard</div>
+    <div className='w-72 h-96 text-black-400 bg-orange-500 rounded-md flex items-center justify-center animate-slide-in-rotate'>BlackCard</div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -65,10 +65,15 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        "slide-in-rotate": {
+          '0%': { transform: 'translate(0, -100rem) rotate(180deg)'},
+          '100%': { transform: ' translate(0, 0rem) rotate(0deg)'},
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "slide-in-rotate": "slide-in-rotate 0.5s ease-in"
       },
     },
   },
