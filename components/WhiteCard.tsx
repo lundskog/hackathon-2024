1dc38eea7751fe182cd2f94f97e08e63e762ac8f
Arvid Kolsmyr <kolsmyrarvid@gmail.com>
2024-01-27 14:17:13 +0100

    added bring to front on card hower from deck

--- components/WhiteCard.tsx
+++ components/WhiteCard.tsx
@@ -5,6 +5,6 @@ import React, { useState } from 'react'
 export default function BlackCard() {
   var int ,[whiteCard, setBlackCard] = useState(0)
   return (
-    <div className='w-72 h-96 -ml-20 border-2 rounded-md border-black text-black bg-slate-100 flex items-center justify-evenly'>WhiteCard</div>
+    <div className='z-0 hover:z-50 w-72 h-96 -ml-20 border-2 rounded-md border-black text-black bg-slate-100 flex items-center justify-evenly'>WhiteCard</div>
   )
 }
